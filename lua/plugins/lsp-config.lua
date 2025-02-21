6a12c9b67c4ccebfaea440eab6f82af98946f9a1
Luis Eduardo Batista <luiseduardob303@gmail.com>
2025-02-21 16:16:27 -0300

    Add lsp for lua

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,36 @@
+local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
+
+later(function()
+  add({ source = "williamboman/mason-lspconfig.nvim", depends = { "williamboman/mason.nvim" } })
+  add({ source = "neovim/nvim-lspconfig" })
+
+  require("mason").setup()
+  require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "basedpyright", "ruff" }
+  })
+
+  local lspconfig = require("lspconfig")
+
+  lspconfig.lua_ls.setup({})
+  lspconfig.basedpyright.setup({})
+  lspconfig.ruff.setup({})
+
+  -- Usando LspAttach para definir keymaps e comportamento apenas quando um LSP estiver ativo
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(event)
+      local opts = { buffer = event.buf }
+
+      -- Keymaps do LSP
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+      vim.keymap.set("n", "cr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+      -- Exibe mensagens inline ao invés de popups
+      vim.diagnostic.config({ virtual_text = true })
+    end,
+  })
+end)
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,64 +1,94 @@
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
-later(function() require("mini.ai").setup() end)
-later(function() require("mini.surround").setup() end)
-later(function() require("mini.pairs").setup() end)
-require("mini.clue").setup()
-require("mini.icons").setup()
-require("mini.statusline").setup()
-require("mini.files").setup({ windows = { preview = true } })
-later(function() require("mini.move").setup() end)
-require("mini.completion").setup({ lsp_completion = {
-	source_func = "omnifunc",
-} })
-require("mini.misc").setup()
-require("mini.extra").setup()
-later(function() require("mini.comment").setup() end)
-require("mini.visits").setup()
-require("mini.bufremove").setup()
-require("mini.hipatterns").setup()
+now(function()
+	require("mini.statusline").setup()
+end)
+now(function()
+	require("mini.visits").setup()
+end)
+now(function()
+	require("mini.icons").setup()
+end)
+now(function()
+	require("mini.files").setup({ windows = { preview = true } })
+end)
+now(function()
+	local function win_config()
+		local height = math.floor(0.618 * vim.o.lines)
+		local width = math.floor(0.618 * vim.o.columns)
+		return {
+			anchor = "NW",
+			height = height,
+			width = width,
+			row = math.floor(0.5 * (vim.o.lines - height)),
+			col = math.floor(0.5 * (vim.o.columns - width)),
+		}
+	end
 
+	require("mini.pick").setup({
+		options = { use_cache = true },
+		window = { config = win_config },
+		mappings = {
+			choose_in_vsplit = "<C-CR>",
+			sys_paste = {
+				char = "<C-v>",
+				func = function()
+					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-r>+", true, true, true), "n", true)
+				end,
+			},
+		},
+	})
+end)
+
+later(function()
+	require("mini.ai").setup()
+end)
+later(function()
+	require("mini.surround").setup()
+end)
+later(function()
+	require("mini.pairs").setup()
+end)
+later(function()
+	require("mini.move").setup()
+end)
+later(function()
+	require("mini.comment").setup()
+end)
+later(function()
+	require("mini.clue").setup()
+end)
+later(function()
+	require("mini.bufremove").setup()
+end)
+later(function()
+	require("mini.extra").setup()
+end)
+
+later(function()
+	require("mini.completion").setup({
+		lsp_completion = { source_func = "omnifunc" },
+	})
+end)
 
 later(function()
 	local hipatterns = require("mini.hipatterns")
 	local hi_words = MiniExtra.gen_highlighter.words
+
 	hipatterns.setup({
 		highlighters = {
 			fixme = hi_words({ "FIXME", "Fixme", "fixme" }, "MiniHipatternsFixme"),
 			hack = hi_words({ "HACK", "Hack", "hack" }, "MiniHipatternsHack"),
 			todo = hi_words({ "TODO", "Todo", "todo" }, "MiniHipatternsTodo"),
 			note = hi_words({ "NOTE", "Note", "note" }, "MiniHipatternsNote"),
-
 			hex_color = hipatterns.gen_highlighter.hex_color(),
 		},
 	})
 end)
 
-local win_config = function()
-	local height = math.floor(0.618 * vim.o.lines)
-	local width = math.floor(0.618 * vim.o.columns)
-	return {
-		anchor = "NW",
-		height = height,
-		width = width,
-		row = math.floor(0.5 * (vim.o.lines - height)),
-		col = math.floor(0.5 * (vim.o.columns - width)),
-	}
-end
-
-require("mini.pick").setup({
-	mappings = {
-		choose_in_vsplit = "<C-CR>",
-	},
-	options = {
-		use_cache = true,
-	},
-	window = {
-		config = win_config,
-	},
-})
-
-vim.ui.select = function(items, opts, on_choice)
-	local start_opts = { window = { config = { width = vim.o.columns } } }
-	return MiniPick.ui_select(items, opts, on_choice, start_opts)
-end
+now(function()
+	vim.ui.select = function(items, opts, on_choice)
+		local start_opts = { window = { config = { width = vim.o.columns } } }
+		return MiniPick.ui_select(items, opts, on_choice, start_opts)
+	end
+end)
